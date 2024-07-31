e86c835ae1980929d78f0a28b791c8c8fc025f0f
KazuMJR <160459045+KazuMJR@users.noreply.github.com>
2024-07-31 16:02:58 -0600

    Clase_abstracta

---
+++ DispositivoElectronico.java
@@ -0,0 +1,25 @@
+abstract class DispositivoElectronico {
+    private String nombre;
+    private String modelo;
+
+    public DispositivoElectronico(String nombre, String modelo) {
+        this.nombre = nombre;
+        this.modelo = modelo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
